71c56e0a7832f57e984947c8cc1dd27eaa7951c3
Bob Atkey <bob.atkey@gmail.com>
2021-09-17 17:18:24 +0100

    [ problems ] Had a problem adding problems

---
+++ lecture-notes/Week01Problems.hs
@@ -0,0 +1,70 @@
+module Week01Problems where
+
+import Week01
+
+{----------------------------------------------------------------------}
+{- Tutorial Questions                                                 -}
+{----------------------------------------------------------------------}
+
+{- In the questions below, replace 'undefined' with your answers. Use
+   GHCi to test them.-}
+
+{- 1. Write a function: -}
+
+isHorizontal :: Direction -> Bool
+isHorizontal = undefined
+
+{- that returns 'True' if the direction is 'Left' or 'Right', and
+   'False' otherwise. -}
+
+
+{- 2. Write a function: -}
+
+flipHorizontally :: Direction -> Direction
+flipHorizontally = undefined
+
+{- that flips horizontally (Left <-> Right, Up and Down stay the same). -}
+
+
+{- 3. Rewrite 'equalDirections' to take a 'Pair Direction Direction' as
+      input: -}
+
+pairOfEqualDirections :: Pair Direction Direction -> Bool
+pairOfEqualDirections = undefined
+
+
+{- 4. Define a datatype 'Triple a b c' for values that have three
+      components. Write functions 'get1of3 :: Triple a b c -> a',
+      'get2of3' and 'get3of3' that return the first, second and third
+      components. You will have to come up with the type signatures
+      for the second and third one. -}
+
+
+{- 5. Pattern matching on specific characters is done by writing the
+      character to match. For example: -}
+
+isA :: Char -> Bool
+isA 'A' = True
+isA _   = False
+
+{-    Write a function 'dropSpaces' :: [Char] -> [Char]' that drops
+      spaces from the start of a list of characters. For example, we
+      should have:
+
+         *Week01> dropSpaces "   hello"
+         "hello"
+
+      (Strings in Haskell are really lists of 'Char's) -}
+
+dropSpaces :: [Char] -> [Char]
+dropSpaces = undefined
+
+{- 6. Using 'reverse' and 'dropSpaces', write a function that removes
+      spaces at the *end* of a list of characters. For example:
+
+         *Week10> dropTrailingSpaces "hello    "
+         "hello"
+-}
+
+dropTrailingSpaces :: [Char] -> [Char]
+dropTrailingSpaces = undefined
