b8f31ddccd9ecd39ab7e72c1fbb8c5561dbc1912
Bob Atkey <bob.atkey@gmail.com>
2021-09-21 12:02:01 +0100

    [ lecture-notes ] Code from Introductory Lecture

--- lecture-notes/Intro.hs
+++ lecture-notes/Intro.hs
@@ -23,6 +23,14 @@ module Intro where
 
 
 
+
+
+
+
+
+
+
+
 {- In this course, you will:
 
      - Learn more about Functional Programming (in Haskell)
@@ -39,6 +47,10 @@ module Intro where
 
    A "Functional Programming Language" is a programming language that
    is designed to make it easy to use Functional Programming ideas. -}
+
+
+
+
 
 
 
@@ -74,17 +86,41 @@ module Intro where
 -}
 
 
+
+
+
+
+
+
+
+
 {- Course arrangements:
 
    - All online
+     Tuesdays at 11:00 : Start the week lecture
+     Fridays  at 14:00 : Tutorial with Georgi Nakov
+     Mondays  at 14:00-15:30 : Open Lab sessions
 
    - Video lectures will "live" coding
+     - ~ 6 videos / week
+     - ~ 10 minutes long
 
    - Online lecture notes in a GitHub repository
+     - git clone https://github.com/bobatkey/CS316-2021.git
+     - git pull
 
    - Tutorial sessions to go through tutorial questions
 
 -}
+
+
+{- This is a programming course
+
+   You will be expected to do a lot of programming in order to understand the concepts.
+   
+   20 credit course : 12 hrs/week, 1 hour of videos, 1 of lectures.
+-}
+
 
 
 
@@ -120,9 +156,89 @@ module Intro where
 
 
 
+
+
 {- Assessment:
 
-   - One class test
+   - One class test (24 hrs) (50%)
+        09:00 Monday 25th October to 09:00 Tuesday 26th October
 
-   - One large coursework
+   - One large coursework "mini-project" (50%)
+        Specification released Week 3 (4th October)
+        Submission Monday 29th November
+
 -}
+
+
+
+
+
+
+
+data Suit
+  = Heart
+  | Club
+  | Spade
+  | Diamond
+  | Circles -- IFPCASG decides this is 'Red'
+  deriving Show
+
+data Colour
+  = Red | Black
+  deriving Show
+
+data Value
+  = Ace | N2 | N3 | N4 | N5 | N6 | N7 | N8 | N9 | N10 | Jack | Queen | King
+  deriving Show
+
+colourOfSuit :: Suit -> Colour   -- public static Colour colourOfSuit(Suit s)
+colourOfSuit Heart   = Red
+colourOfSuit Diamond = Red
+-- colourOfSuit _       = Black
+colourOfSuit Club    = Black
+colourOfSuit Spade   = Black
+colourOfSuit Circles = Red
+
+numericValue :: Value -> Integer
+numericValue Ace   = 1
+numericValue N2    = 2
+numericValue N3    = 3
+numericValue N4    = 4
+numericValue N5    = 5
+numericValue N6    = 6
+numericValue N7    = 7
+numericValue N8    = 8
+numericValue N9    = 9
+numericValue N10   = 10
+numericValue Jack  = 11
+numericValue Queen = 12
+numericValue King  = 13
+
+-- Computing the ordering between card values by:
+--   1. Translating from card values to Integers
+--   2. Comparing the integers
+greaterValue :: Value -> Value -> Bool
+greaterValue v1 v2 = numericValue v1 > numericValue v2
+
+greaterOrEqualValue :: Value -> Value -> Bool
+-- greaterOrEqualValue v1 v1 = True -- not allowed
+greaterOrEqualValue v1 v2 = numericValue v1 >= numericValue v2
+
+data Card = MkCard Suit Value
+  deriving Show
+
+getSuit :: Card -> Suit
+getSuit (MkCard suit value) = suit
+
+-- getSuit (MkCard Heart N7)
+
+
+-- For you to do:
+
+-- getValue :: Card -> Value
+
+-- numericOfSuit :: Suit -> Integer
+
+-- greaterSuit :: Suit -> Suit -> Bool
+
+-- greaterCard :: Card -> Card -> Bool
